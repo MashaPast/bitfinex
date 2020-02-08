fb7ceb7a3b7259ac7e0979d25c2ee218a2a33c68
MashaPast <mariepast13@gmail.com>
2020-02-08 20:12:46 +0200

    if response is a list, types of field are checked

--- test_endpoints.py
+++ test_endpoints.py
@@ -1,6 +1,6 @@
 from logger import appLogger
 import os
-from typing import Dict, Union
+from typing import Dict
 import json
 from endpoints import Endpoint
 import requests
@@ -13,7 +13,7 @@ import time
 
 #('tickers?symbols=tBTCUSD'), ('tickers?symbols=ALL'), ('ticker/fUSD'), ('trades/tBTCUSD/hist'))
 def test_response_code_time_content_type(endpoint):
-    appLogger.debug('Getting response from 5 endpoints to check response code from Bitfinex API is ok')
+    appLogger.debug('Getting response to check if response code from Bitfinex API is ok')
     each_endpoint = Endpoint().create_new_endpoint(endpoint)
 
     start = time.clock()
@@ -21,7 +21,7 @@ def test_response_code_time_content_type(endpoint):
     response_time = time.clock() - start
     appLogger.debug("Request completed in {}ms".format(response_time))
 
-    appLogger.debug('Checking asserts for 5 endpoints if response code is ok')
+    appLogger.debug('Check if response code is ok')
     assert response.ok
 
     assert response_time < 1
@@ -30,26 +30,42 @@ def test_response_code_time_content_type(endpoint):
     appLogger.debug('Getting response headers from Bitfinex API')
     with open(os.path.abspath('assets/content_type.json'), 'r') as f:
         content_type: Dict = json.load(f)
-        appLogger.debug('Checking asserts for 5 endpoints if content-type is json')
+        appLogger.debug('Check if content-type is json')
         assert headers_data['Content-Type'] == content_type['Content-Type']
+
+    body: list = response.json()
+    appLogger.debug('Check if response type is list')
+    assert isinstance(body, list)
+
+def write_response_to_a_file(response_data, file_name):
+    file = open(file_name, "w")
+    file.write(response_data.text)
+    file.close()
 
 
 def test_response_body_field_types_endpoint1():
     each_endpoint = Endpoint().create_new_endpoint('platform/status')
     response = requests.get(each_endpoint)
-    body: Dict = response.json()
+    body: list = response.json()
+    appLogger.debug('Writing response body to a file')
+    write_response_to_a_file(response, "resp_text1.txt")
     appLogger.debug('Getting response body from Bitfinex API')
-    print(body[0])
     assert isinstance(body[0], int)
 
 
 def test_response_body_field_types_endpoint2():
-    each_endpoint = Endpoint().create_new_endpoint('tickers?symbols=tBTCUSD,tLTCUSD,fUSD')
+    each_endpoint = Endpoint().create_new_endpoint('tickers?symbols=tBTCUSD')
     response = requests.get(each_endpoint)
-    body: Dict = response.json()
-    appLogger.debug('Getting response body from Bitfinex API')
-    print(body[0][0])
-    assert isinstance(body[0][0], str)
-    assert isinstance(body[0][1], float)
-    assert isinstance(body[0][2], float)
+    body: list = response.json()
+    #list_of_types = [str, float, float, str, float, float, str, float, float, str, float, float]
+    appLogger.debug('Writing response body to a file')
+    write_response_to_a_file(response, "resp_text2.txt")
+    for i in range(0, len(body)):
+        for j in range(0, len(body[i])):
+            a = (type(body[i][j]))
+            appLogger.debug('Check type')
+            assert a is str
+    # assert isinstance(body[0][0], str)
+    # assert isinstance(body[0][1], float)
+    # assert isinstance(body[0][2], float)
 
